7aeb34dda843b11433eb663867ea117707cfc0cf
periclescy <periclescy@outlook.com>
2022-05-26 12:53:22 +0300

    Embed YouTube video on index page.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <h1 class="display-4 text-center py-3">DESCANT Demonstration Tool</h1>
         <div class="row">
             <div class="col-md-6">
-                <p class="pb-4 text-justify">
+                <p class="text-justify">
                     This tool was developed in the framework of the DESCANT:
                     “Detecting Stereotypes in Human Computational Tasks” research project funded by
                     the Research and Innovation Foundation of Cyprus under the RESTART 2016-2020-EXCELLENCE HUBS call.
@@ -12,8 +12,23 @@
                     <a href="https://www.cyens.org.cy/en-gb/research/projects/descant-detecting-stereotypes-in-human-computati" target="_blank">project’s website</a>.
                 </p>
 
+                <div class="mb-3 ratio ratio-16x9">
+                    <iframe src="https://www.youtube.com/embed/hsALsdx82Zo?controls=0" title="YouTube video player" allowfullscreen></iframe>
+                </div>
+
+                <h5>What does the tool do?</h5>
+                <p class="text-justify">
+                    The tool allows users to explore the ways in which the characteristics of the training dataset
+                    affect the resulting machine learning models. By selecting an image and classification task,
+                    the tool presents the performance results of the machine learning models trained
+                    using data annotated by different groups of annotators.
+                </p>
+
+            </div>
+
+            <div class="col-md-6">
                 <h5>What dataset does the tool use?</h5>
-                <p class="pb-4 text-justify">
+                <p class="text-justify">
                     The <a href="https://www.chicagofaces.org" target="_blank">Chicago Face Database (CFD)</a>
                     was developed at the University of Chicago by Debbie S. Ma, Joshua Correll, and Bernd Wittenbrink.
                     The CFD is intended for use in scientific research. It provides high-resolution,
@@ -27,17 +42,6 @@
                     with happy (open mouth), happy (closed mouth), angry, and fearful expressions.
                     Norming data are available for all neutral expression images.
                     Subjective rating norms are based on a U.S. rater sample.
-                </p>
-
-            </div>
-            <div class="col-md-6">
-
-                <h5>What does the tool do?</h5>
-                <p class="pb-4 text-justify">
-                    The tool allows users to explore the ways in which the characteristics of the training dataset
-                    affect the resulting machine learning models. By selecting an image and classification task,
-                    the tool presents the performance results of the machine learning models trained
-                    using data annotated by different groups of annotators.
                 </p>
 
                 <h5>What machine learning modeling techniques are used?</h5>
